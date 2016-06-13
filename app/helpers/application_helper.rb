817e12679863e623654547fb0300fd5b319cbc64
Rob Cornish <rob@robcornish.net>
2016-06-13 22:49:30 +1000

    added coffee consumed stats to index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,33 @@ module ApplicationHelper
   def format_date(time)
     time.strftime('%A %d %B %Y')
   end
+
+  def seven_days_ago
+    Time.zone.now - 7.days
+  end
+
+  def number_of_coffees_last_seven_days
+    CoffeeConsumption.last_7_days.count
+  end
+
+  def total_coffees_drunk
+    CoffeeConsumption.all.count
+  end
+
+  def total_caffine_consumed
+    CoffeeConsumption.consumed_caffine
+  end
+
+  def formatted_total_caffine_consumed
+    "#{total_caffine_consumed} mg"
+  end
+
+  def caffine_consumed_last_7_days
+    CoffeeConsumption.last_7_days.consumed_caffine
+  end
+
+  def formatted_caffine_consumed_last_7_days
+    "#{caffine_consumed_last_7_days} mg"
+  end
+
 end
--- app/models/coffee_consumption.rb
+++ app/models/coffee_consumption.rb
@@ -1,7 +1,17 @@
 class CoffeeConsumption < ActiveRecord::Base
+
   belongs_to :coffee_type
   has_one :coffee_note, dependent: :destroy
 
   enum size: [:half, :single, :double, :triple]
+
+
+  def self.last_7_days
+    where(date: ((Time.now - 7.days)..Time.now))
+  end
+
+  def self.consumed_caffine
+    pluck(:caffine_in_mg).sum
+  end
 
 end
